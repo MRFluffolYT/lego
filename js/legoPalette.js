d3d8cbe14f77276f1f6ff5e707304950893adb86
MRFluffolYT <86803116+MRFluffolYT@users.noreply.github.com>
2026-01-29 12:15:08 +0100

    Add LEGO_PALETTE with color definitions

---
+++ js/legoPalette.js
@@ -0,0 +1,8 @@
+export const LEGO_PALETTE = [
+  { name: "White", rgb: [242,243,242] },
+  { name: "Black", rgb: [27,42,52] },
+  { name: "Red", rgb: [196,40,27] },
+  { name: "Blue", rgb: [13,105,171] },
+  { name: "Yellow", rgb: [245,205,47] },
+  { name: "Dark Gray", rgb: [109,110,108] }
+];
