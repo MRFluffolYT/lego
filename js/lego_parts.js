c04a2fb8f2a4bc4e5fb7ed636a8f86f3a49bc624
MRFluffolYT <86803116+MRFluffolYT@users.noreply.github.com>
2026-01-29 12:39:09 +0100

    Add index numbers to LEGO 1x1 parts

--- js/lego_parts.js
+++ js/lego_parts.js
@@ -1,11 +1,11 @@
 window.LEGO_1x1_PARTS = [
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Red","colorID":"14","elementID":"3024013","rgb":[201,26,9],"hex":"#C91A09" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"White","colorID":"1","elementID":"3024011","rgb":[255,255,255],"hex":"#FFFFFF" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Black","colorID":"5","elementID":"3024002","rgb":[5,19,29],"hex":"#05131D" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Blue","colorID":"11","elementID":"3024014","rgb":[0,85,191],"hex":"#0055BF" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Yellow","colorID":"24","elementID":"3024015","rgb":[245,205,47],"hex":"#F5CD2F" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Green","colorID":"28","elementID":"3024016","rgb":[35,120,65],"hex":"#237841" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Dark Gray","colorID":"23","elementID":"3024017","rgb":[109,110,92],"hex":"#6D6E5C" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Light Gray","colorID":"8","elementID":"3024018","rgb":[155,161,157],"hex":"#9BA19D" },
-  { "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Orange","colorID":"25","elementID":"3024019","rgb":[255,128,13],"hex":"#FF800D" }
+  { "number":1, "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Red","colorID":"14","elementID":"3024013","rgb":[201,26,9],"hex":"#C91A09" },
+  { "number":2, "designID":"3024","partName":"Plate 1 x 1","colorName":"White","colorID":"1","elementID":"3024011","rgb":[255,255,255],"hex":"#FFFFFF" },
+  { "number":3, "designID":"3024","partName":"Plate 1 x 1","colorName":"Black","colorID":"5","elementID":"3024002","rgb":[5,19,29],"hex":"#05131D" },
+  { "number":4, "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Blue","colorID":"11","elementID":"3024014","rgb":[0,85,191],"hex":"#0055BF" },
+  { "number":5, "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Yellow","colorID":"24","elementID":"3024015","rgb":[245,205,47],"hex":"#F5CD2F" },
+  { "number":6, "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Green","colorID":"28","elementID":"3024016","rgb":[35,120,65],"hex":"#237841" },
+  { "number":7, "designID":"3024","partName":"Plate 1 x 1","colorName":"Dark Gray","colorID":"23","elementID":"3024017","rgb":[109,110,92],"hex":"#6D6E5C" },
+  { "number":8, "designID":"3024","partName":"Plate 1 x 1","colorName":"Light Gray","colorID":"8","elementID":"3024018","rgb":[155,161,157],"hex":"#9BA19D" },
+  { "number":9, "designID":"3024","partName":"Plate 1 x 1","colorName":"Bright Orange","colorID":"25","elementID":"3024019","rgb":[255,128,13],"hex":"#FF800D" }
 ];
